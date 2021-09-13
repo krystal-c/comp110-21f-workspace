b497321870d24c66072c75fe1fd10b6c6f505aaf
Krystal Donovan <kdonovan2@unc.edu>
2021-09-13 08:30:06 -0400

    count and function save

--- exercises/ex02/count_letters.py
+++ exercises/ex02/count_letters.py
@@ -9,11 +9,40 @@ sentence: str = str(input("Enter a word: "))
 count = 0
 
 for i in sentence:
-    if i == "A":
+    if i == "a":
         count = count + 1
-    elif i == "a":
+    elif i == "b":
         count = count + 1
-    elif i == "B": 
+    elif i == "c": 
         count = count + 1
+    elif i == "d":
+        count = count + 1
+    elif i == "e":
+        count = count + 1
+    elif i == "f":
+        count = count + 1
+    elif i == "g":
+        count == count + 1
+    elif i == "h":
+        count = count + 1
+    elif i == "i":
+        count = count + 1
+    elif i == "j":
+        count = count + 1
+    elif i == "k":
+        count = count + 1
+    elif i == "l":
+        count = count + 1
+    elif i == "m":
+        count = count + 1
+    elif i == "n":
+        count = count + 1
+    elif i == "o":
+        count = count + 1
+    elif i == "p":
+        count = count + 1
+    elif i == "q":
+        count = count + 1
+    
 
 print(count)
